87c4e8bf8482d9d2ea8008de4effe531c1a4816a
vagrant <vagrant@vagrant.vm>
2017-12-18 04:15:57 +0000

    Add header html pages and routing

--- project.py
+++ project.py
@@ -29,10 +29,26 @@ def month(year, month):
 	dates = session.query(Date).filter_by(year = year, month = month).all()
 	return render_template('month.html', year=year, monthobj=monthobj, dates=dates)
 
-@app.route('/calendates/<int:year>/<int:month>/<int:date>', methods=['GET', 'POST'])
+@app.route('/calendates/<int:year>/<int:month>/<int:date>/', methods=['GET', 'POST'])
 def date(year, month, date):
 	date = session.query(Date).filter_by(year=year, month=month, date=date).one()
 	return render_template('date.html', date = date)
+
+@app.route('/calendates/events/', methods=['GET', 'POST'])
+def events():
+	return render_template('events.html')
+
+@app.route('/calendates/newevent/', methods=['GET', 'POST'])
+def newEvent():
+	return render_template('newevent.html')
+
+@app.route('/calendates/about/', methods=['GET', 'POST'])
+def about():
+	return render_template('about.html')
+
+@app.route('/calendates/contact/', methods=['GET', 'POST'])
+def contact():
+	return render_template('contact.html')
 
 ## For running website on localhost:5000 in debug mode (automatic refresh of webserver on file save)
 if __name__ == '__main__':
