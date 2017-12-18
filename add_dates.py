ed716e8adf258bc0565e1d629d5c4e9285733162
vagrant <vagrant@vagrant.vm>
2017-12-18 09:00:29 +0000

    manually add public holidays via add_dates

--- add_dates.py
+++ add_dates.py
@@ -10,7 +10,6 @@ session = DBSession()
 
 #function to add dates for whole year. Input the year and the day for the 1st of that year (eg. 'Monday')
 def createMonthsForYear(year, day):
-	print "Adding year " + str(year) + " to database."
 	for x in range(1, 13):
 		day = createDatesForMonth(year, (x), day)
 	print "Added year " + str(year)
@@ -90,7 +89,39 @@ def getMonthName(month):
 	elif month == 12:
 		return 'December'
 
+''' Functions to create Events once Dates have been added '''
+def createPublicHolidays2018():
+	newYearsDayDate = session.query(Date).filter_by(year=2018, month=1, date=1).one()
+	newYearsDay = Event(name="New Year's Day", date_id=newYearsDayDate.id, description='The day after the new year.', country='England', city='London')
+	session.add(newYearsDay)
+	goodFriDate = session.query(Date).filter_by(year=2018, month=3, date=30).one()
+	goodFri = Event(name="Good Friday", date_id=goodFriDate.id, description='Friday for Easter', country='England', city='London')
+	session.add(goodFri)
+	easterMonDate = session.query(Date).filter_by(year=2018, month=4, date=2).one()
+	easterMon = Event(name='Easter Monday', date_id=easterMonDate.id, description='The Monday during Easter', country='England', city='London')
+	session.add(easterMon)
+	earlyMayBankDate = session.query(Date).filter_by(year=2018, month=5, date=7).one()
+	earlyMayBank = Event(name='Early May Bank Holiday', date_id=earlyMayBankDate.id, description='Bank holiday for early May', country='England', city='London')
+	session.add(earlyMayBank)
+	springBankDate = session.query(Date).filter_by(year=2018, month=5, date=28).one()
+	springBank = Event(name='Spring Bank Holiday', date_id=springBankDate.id, description='Bank holiday for Spring', country='England', city='London')
+	session.add(springBank)
+	summerHolDate = session.query(Date).filter_by(year=2018, month=8, date=27).one()
+	summerHol = Event(name='Summer Bank Holiday', date_id=summerHolDate.id, description='A public holiday in Summer for banks', country='England', city='London')
+	session.add(summerHol)
+	xmasDayDate = session.query(Date).filter_by(year=2018, month=12, date=25).one()
+	xmasDay = Event(name='Christmas Day', date_id=xmasDayDate.id, description='Christmas is here!', country='England', city='London')
+	session.add(xmasDay)
+	boxingDayDate = session.query(Date).filter_by(year=2018, month=12, date=26).one()
+	boxingDay = Event(name='Boxing Day', date_id=boxingDayDate.id, description='The day after Christmas', country='England', city='London')
+	session.add(boxingDay)
+	session.commit()
+	print "Added 2018 London public holidays."
+
+
+print "Adding data to database..."
 createMonthsForYear(2018, 'Monday')
 createMonthsForYear(2019, 'Tuesday')
 createMonthsForYear(2020, 'Wednesday')
+createPublicHolidays2018()
 print "Completed."
