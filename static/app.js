1c1d7b8506f68cbb7e1527809e089231083e7376
vagrant <vagrant@vagrant.vm>
2017-12-20 22:13:42 +0000

    Add visibility toggle for edit form

--- static/app.js
+++ static/app.js
@@ -1,12 +1,21 @@
 (function() {
 	
-	document.getElementById("end-date-checkbox").addEventListener("click", function() {
-		var endDateContainer = document.getElementById("end-date-container");
+	document.getElementById('end-date-checkbox').addEventListener('click', function() {
+		var endDateContainer = document.getElementById('end-date-container');
 		if (this.checked) {
-			endDateContainer.style.display = "block";
+			endDateContainer.style.display = 'block';
 		} else {
-			endDateContainer.style.display = "none";
+			endDateContainer.style.display = 'none';
 		}
+	});
+
+	document.getElementById('edit-btn').addEventListener('click', function() {
+		var editForm = document.getElementById('edit-event');
+		if (editForm.style.visibility == 'visible') {
+			editForm.style.visibility = 'hidden';
+		} else {
+			editForm.style.visibility = 'visible';
+		};
 	});
 
 })();
